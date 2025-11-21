e475d21fec27ee223a51168b220fd84205ff7b3d
Pedro Luis Dias <luisp.diias@gmail.com>
2025-11-21 16:49:12 -0300

    add ejs template render

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,36 @@
 import {Server} from "http";
 import ejs from 'ejs';
 
+const viewsPath = 'src/views/';
+const routes =  [
+    {'path': '/', 'template': 'index.ejs'},
+
+]
+
 const server = new Server((req, res) => {
+
+    const route = routes.find(r => r.path === req.url);
+    if (route) {
+        ejs.renderFile(`${viewsPath}${route.template}`, {}, (err, str) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(str);
+            }
+        });
+    } else {
+        ejs.renderFile(`${viewsPath}404.ejs`, {}, (err, str) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end(str);
+            }
+        });
+    }
     
 });
 
