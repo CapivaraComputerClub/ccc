bcfde853fa34f4fb442998c62c05846fda6150ec
Pedro Luis Dias <luisp.diias@gmail.com>
2025-11-21 21:49:16 -0300

    Change default server port from 80 to 3000

--- src/index.mjs
+++ src/index.mjs
@@ -98,7 +98,7 @@ const server = new Server((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`running at http://0.0.0.0:${PORT}/`);
 });
