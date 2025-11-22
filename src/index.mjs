fa816edc71ad911a23e1d7c197687e50dc0cad97
Pedro Luis Dias <luisp.diias@gmail.com>
2025-11-21 21:18:47 -0300

    Add Dockerfile and configuration for deployment

--- src/index.mjs
+++ src/index.mjs
@@ -99,6 +99,6 @@ const server = new Server((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}/`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`running at http://0.0.0.0:${PORT}/`);
 });
